f06bad1a36ae07e97fe5501ef40ddc48a0990980
Kateryna Kustova <katerynakustova@MacBook-Air-Kateryna.local>
2024-11-06 15:55:47 +0200

    eslint checking

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'semi': ['error', 'always'] 
+    }
+      
+  },
   pluginJs.configs.recommended,
 ];
